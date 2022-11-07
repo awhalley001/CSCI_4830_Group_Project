0c1f429b05c9d66fa5e47f6ae0daddbac5444298
ahalley001 <ahalley001@gmail.com>
2022-11-06 20:33:20 -0600

    changes to home/urls.py and home/views.py

--- Yard_Scheduler/Yard_Scheduler/settings.py
+++ Yard_Scheduler/Yard_Scheduler/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'home',
+    'yards',
+    'yard_capacity',
+    'track_capacity',
+
 ]
 
 MIDDLEWARE = [
