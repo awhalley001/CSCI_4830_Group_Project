c5586c2b51e3df4b01b0e25a9713b645e2de1627
ahalley001 <ahalley001@gmail.com>
2022-11-06 22:25:45 -0600

    first successful run local server for home.html!

--- Yard_Scheduler/home/urls.py
+++ Yard_Scheduler/home/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('about/', views.about),
-    path('problem_statement/', views.problem_statement),
-    path('software_design_document/', views.problem_statement)
+    # path('about/', views.about),
+    # path('problem_statement/', views.problem_statement),
+    # path('software_design_document/', views.problem_statement),
+    path('index/', views.home)
 ]
