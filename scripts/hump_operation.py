809220e8c225ebe349c8b3fb8509ea3b855f8325
ahalley001 <ahalley001@gmail.com>
2022-11-09 20:11:47 -0600

    addedhumperoperation to scripts for chris and thomas

--- scripts/hump_operation.py
+++ scripts/hump_operation.py
@@ -1,22 +1,38 @@
 import mysql
+import csv
+from timer.py import TimeClass
 
+
+def get_track_csv():
+    '''Get tracks from user uploaded csv'''
+    pass
+
+def connect_to_database():
+    '''Connect to local database'''
+    #TODO connect to aws via django
 
 def import_csv_to_table():
+    '''Use csv contents to populate table in database'''
     pass
  
 def parse_cars_by_track():
+    '''Add cars to track dictionary where Key=Trackname Value=Carnumber '''
     pass
     
-def hump_next_car():
+def run_hump_timer():
+    '''execute hump timer to time car movement into track and update track capacity'''
     pass
     
 def update_track_capacity():
+    '''update capacity column in database depending on where the cars are going'''
     pass
 
 def is_track_full():
+    '''Return if a track is full'''
     pass
     
 def try_next_track():
+    '''Search for nearest track to move overfill from previous track'''
     pass
 
 def main():
