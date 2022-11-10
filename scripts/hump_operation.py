e99e9d9666d0cbbb1a42e183bac4cbab452bdd02
ahalley001 <ahalley001@gmail.com>
2022-11-09 20:11:44 -0600

    added hump_operation script for Thomas and Chris

---
+++ scripts/hump_operation.py
@@ -0,0 +1,26 @@
+import mysql
+
+
+def import_csv_to_table():
+    pass
+ 
+def parse_cars_by_track():
+    pass
+    
+def hump_next_car():
+    pass
+    
+def update_track_capacity():
+    pass
+
+def is_track_full():
+    pass
+    
+def try_next_track():
+    pass
+
+def main():
+    pass
+    
+if __name__ = '__main__':
+    main()
