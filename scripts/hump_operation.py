b0d4dcee58d6108efa5373bc95068892cb52d310
chrisrecino <crecinos@unomaha.edu>
2022-11-15 12:37:08 -0600

    local db use + pandas
    updating dataframe with logic, still need to implement updating the db with dataframe changes

--- scripts/hump_operation.py
+++ scripts/hump_operation.py
@@ -59,81 +59,103 @@ def connect_to_database():
         password="jollyStarter22",
         database="yardTracks"
     )
+
     if mydb:
         return mydb
     else:
         exit("Problem connecting to db")
 
 
-def get_incoming_cars_id(cardata):
-    '''Get the user uploaded csv file that includes the cars that need to be moved'''
-    # "id" or "EQUIPMENT_NUMBER"
-    cars = cardata["id"]
-    return
-
-
-def get_incoming_cars_track():
-    '''Get the respective tracks for each of the incoming cars.'''
-    pass
-
-
-def get_cars_csv(fn):
-    '''Get the cars csv from database.'''
-    df = pandas.read_csv(fn)
+def get_cars_data(db):
+    '''Get the cars table from database.'''
+    cursor = db.cursor()
+    cursor.execute("SELECT id, EQUIPMENT_INITIAL, EQUIPMENT_NUMBER, CURRENT_YARD, CURRENT_TRAIN_DATE FROM testCars")
+    df = pandas.DataFrame(cursor.fetchall())
+    df.columns=['id', 'EQUIPMENT_INITIAL', 'EQUIPMENT_NUMBER', 'CURRENT_YARD', 'CURRENT_TRAIN_DATE']
     return df
 
 
-def get_track_csv():
-    '''Get the tracks csv from database.'''
-    pass
+def get_track_data(db):
+    '''Get the tracks table from database.'''
+    cursor = db.cursor()
+    cursor.execute("SELECT id,SBDV_NAME,TRK_SYS_NBR,dist,car_capacity FROM testYard")
+    df = pandas.DataFrame(cursor.fetchall())
+    df.columns=['id', 'SBDV_NAME', 'TRK_SYS_NBR', 'dist', 'car_capacity']
+    return df
 
 
-def is_track_full():
+def get_incoming_cars_track(yarddata):
+    '''Get the respective tracks for each of the incoming cars.'''
+    # "id" or "TRK_SYS_NBR"
+    tracks = yarddata["id"]
+    return tracks
+
+
+def get_cars_csv(filename):
+    '''Get the cars csv from uploaded user file.'''
+    df = pandas.read_csv(filename)
+    return df
+
+
+def is_track_full(dbyard,trackid):
     '''Returns true if the track is full. Returns false otherwise.'''
-    pass
+
+    if dbyard.at[trackid, 'car_capacity'] == 0:
+        return True
+    else:
+        return False
 
 
 def run_hump_timer():
     '''Execute hump timer to time car mocement into track and update track capacity.'''
-    pass
+    print("time passes")
 
 
-def update_track_capacity():
+
+def update_track_capacity(dbyard, trackid):
     '''Update the capacity column of the given track in the database.'''
-    pass
+    currentcapacity = int(dbyard.at[trackid, 'car_capacity'])
+    print(dbyard.loc[[trackid]])
+    dbyard.at[trackid, 'car_capacity'] = currentcapacity - 1
+    print(dbyard.loc[[trackid]])
+
+    #still need to update db
+    ...
+    return dbyard
 
 
-def update_car_location():
+def update_car_location(car, dbcars):
     '''Update the location column of the given car in the database.'''
+    print("car data gets added to the dbcars dataframe and the db gets updated")
     pass
 
 
 def main():
-    cardata = get_cars_csv("CAR_DATA1.csv")
+    #dataframe will be referred to as df
+
+    # returns pandas df of car_data.txt file
+    uploadedcardata = get_cars_csv("CAR_DATA1.csv")
+
     db = connect_to_database()
-    cursor = db.cursor()
+    # returns pandas df of testYard table in db
+    dbyard = get_track_data(db)
+    # returns pandas df of testCars table in db
+    dbcars = get_cars_data(db)
 
-    # Example of sql statement execution
-    cursor.execute("SHOW TABLES")
-    for x in cursor:
-        print(x)
+    # returns column from yarddata dataframe
+    car_tracks = get_incoming_cars_track(dbyard)
 
+    for x in range(len(uploadedcardata.index)):
+        car = uploadedcardata.iloc[x]
+        to_track = car_tracks[x]
 
-    #get_track_csv()
-    car_ids = get_incoming_cars_id(cardata)
-    car_tracks = get_incoming_cars_track()
+        while is_track_full(dbyard, to_track):
+            to_track = car_tracks[x + 1]
 
-    #for x in range(len(car_ids)):
-    #    car = car_ids[x]
-    #    to_track = car_tracks[x]
-    #
-    #    while is_track_full(to_track):
-    #        to_track = car_tracks[x + 1]
-    #
-    #    run_hump_timer()
-    #
-    #    update_track_capacity(to_track)
-    #    update_car_location(car)
+        run_hump_timer()
+
+        dbyard = update_track_capacity(dbyard, to_track)
+        update_car_location(car, dbcars)
 
 if __name__ == '__main__':
     main()
